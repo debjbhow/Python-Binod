d628fae86c60e03f6fa72d21654ec96ee77d049e
Debjit Bhowmik <debjitbhowmik807@gmail.com>
2020-08-19 11:47:50 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+
+def isBinod(filename):
+    with open(filename, "r") as f:
+        fileContent = f.read()
+    #Detect all forms of Binod like BInoD
+    if "binod" in fileContent.lower():
+        return True
+    else:
+        return False
+
+
+
+if __name__ == "__main__":
+    #Listing the contents of this folder        
+    dir_contents = os.listdir()
+
+    nBinod = 0
+    #For each text file, run isBinod for them
+    for item in dir_contents:
+        if item.endswith('txt'):
+            print(f"Detecting Binod in {item}....")
+            flag = isBinod(item)
+            if(flag):
+                print(f"*****Binod found in {item}!!!!!!!")
+                nBinod +=1
+            else:
+                print(f"Binod not found in {item}")
+    
+    print("*******Binod Detector Summary**********")
+    print(f"{nBinod} files found with Binod hidden into them")
